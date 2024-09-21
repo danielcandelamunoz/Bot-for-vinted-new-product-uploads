86e1be7128c379ee193edd5cda834ea3998e909c
danielcandelamunoz <daniel.candelamunoz@usp.ceu.es>
2024-09-21 18:04:15 +0200

    render.yaml para instalar google chrome

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ async def save_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
         
 def get_driver(): 
     chrome_options = Options()
-    chrome_options.add_argument("--disable-cache")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920x1080")
